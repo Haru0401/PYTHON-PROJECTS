f0014a9b7cf5731334ca96890f6d1699f56f6162
Haru0401 <108670451+Haru0401@users.noreply.github.com>
2024-07-20 18:58:43 +0530

    first commit

---
+++ new.py
@@ -0,0 +1,15 @@
+
+def variable():
+    a=input('lets try')
+    print("hello provided  ",a)
+    a=0
+    b=int('6592')
+    c=a+b
+    klkl
+    print(type(c),c)
+
+if 9<0:
+    print('nope')
+else:
+    print('yeah')
+variable()
